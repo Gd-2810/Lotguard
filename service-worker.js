a8b55fe60e7741ba18cb58102b09ea1ba37fdf79
Gd-2810 <george93daniel@gmail.com>
2026-08-15 21:20:50 -0700

    Fix PWA feed polling to 5 seconds for near-instant updates

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // index.html: network-first (always fresh)
 // Static assets: cache-first
 
-const CACHE_NAME = 'lotguard-v16';
+const CACHE_NAME = 'lotguard-v17';
 const STATIC_ASSETS = [
     '/manifest.json',
     '/icon-512.png'
